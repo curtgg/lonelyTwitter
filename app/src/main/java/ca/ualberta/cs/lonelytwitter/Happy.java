2358758674c86a50f8bdbda93843add7180d8d63
curtis goud <cgoud@ualberta.ca>
2018-09-19 14:43:34 -0600

    Implementation of Abstract mood class and subclasses

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Happy.java
@@ -0,0 +1,27 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+public class Happy extends Mood {
+
+    public Happy(){
+        super();
+    }
+
+    public Happy(Date dateTime){
+        super(dateTime);
+    }
+
+    public Date getDate(){
+        return this._DateTime;
+    }
+
+    public void setDate(Date dateTime){
+        this._DateTime = dateTime;
+    }
+
+    public String formatMood(){
+        return "Feeling really happy at: " + getDate();
+    }
+
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Sad.java
@@ -0,0 +1,27 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+public class Sad extends Mood {
+
+    public Sad(){
+        super();
+    }
+
+    public Sad(Date dateTime){
+        super(dateTime);
+    }
+
+    public Date getDate(){
+        return this._DateTime;
+    }
+
+    public void setDate(Date dateTime){
+        this._DateTime = dateTime;
+    }
+
+    public String formatMood(){
+        return "Feeling really sad at: " + getDate();
+    }
+
+}
